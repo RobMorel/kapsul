19f70416adc85207991a3313ffd76fcd9acc0cad
robert morel <robertmorel7373@gmail.com>
2024-12-09 16:43:55 +0100

    Creating tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_09_153504) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_09_154152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_09_153504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_capsules_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "capsule_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["capsule_id"], name: "index_comments_on_capsule_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "like"
+    t.bigint "capsule_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["capsule_id"], name: "index_likes_on_capsule_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +58,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_09_153504) do
   end
 
   add_foreign_key "capsules", "users"
+  add_foreign_key "comments", "capsules"
+  add_foreign_key "likes", "capsules"
 end
