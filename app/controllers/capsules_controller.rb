e14f88ebfc1e064ef5eb072a6b339139d5866f13
abdelhamid ameziane <ameziane001@gmail.com>
2024-12-10 16:05:07 +0100

    setup mapbox

--- app/controllers/capsules_controller.rb
+++ app/controllers/capsules_controller.rb
@@ -1,2 +1,15 @@
 class CapsulesController < ApplicationController
+
+#bande de loser dans cette methode je recupère la liste de toutes les capsules et un hash de leurs coordonnées
+
+def index
+    @capsules = Capsule.all
+    @markers = Capsule.geocoded.map do |capsule|
+      {
+        lat: capsule.latitude,
+        lng: capsule.longitude
+      }
+    end
+  end
+
 end
