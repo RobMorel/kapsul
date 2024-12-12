6436035b5929a6de4bbb93bef13941fb58546c03
juan descoux <juan.descoux@hotmail.fr>
2024-12-12 12:25:39 +0100

    modif_form_add_input_upload_image

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -9,6 +9,8 @@ class Capsule < ApplicationRecord
   validates :category, presence: true
   validates :address, presence: true
 
+  has_one_attached :photo
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
