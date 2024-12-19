39e9d52229b5f745d6b51690727854bacebbeee8
robert morel <robertmorel7373@gmail.com>
2024-12-19 09:52:46 +0100

    address autocomplete and capsules styling

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,6 +18,8 @@ export default class extends Controller {
     //this.trackUserPosition();
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
 
   initMapbox() {
